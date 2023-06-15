a28dbe5a37a948632da8e645e8e1eba995eedb0e
Sohail Charef <charefsohayl@gmail.com>
2023-06-15 14:19:27 -0700

    Another one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,8 @@
 
 int main(void)
 {
-	const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(quote) - 1;
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, len);
+	write(2, quot, 59);
 	return (1);
 }
