ea3462b75488c4f2db92bd228143b8e4acbe2a80
Sohail Charef <charefsohayl@gmail.com>
2023-06-15 14:05:11 -0700

    Another one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programming that prints a quote using write function.
+ *    ssize_t write(int fd.const void *buf.size_t count)
+ *
+ * Return: Always 1 (Not success)
+*/
+
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
