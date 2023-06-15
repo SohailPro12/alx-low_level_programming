cc57cb9b9d60edd375903dac3606ba9dbb2352e9
Sohail Charef <charefsohayl@gmail.com>
2023-06-15 08:22:42 -0700

    Another commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C programming that print with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
