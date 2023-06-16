6f8d10b8a2390eaa4f56ddc1b30d35eb59c3e24a
Sohail Charef <charefsohayl@gmail.com>
2023-06-16 03:53:55 -0700

    Another one

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: numbers using char, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
