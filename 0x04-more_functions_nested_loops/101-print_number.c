4a6489b38e7bdc5fd6dc62d948036f1f71643704
Sohail Charef <charefsohayl@gmail.com>
2023-06-22 13:08:02 -0700

    finally

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 
 
-	if (num / 10 != 0)
+	if ((num / 10) > 0)
 		print_number(num / 10);
 
 
