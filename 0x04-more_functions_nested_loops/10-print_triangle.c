1b57ece9e5cc264ae6ae3025439a36f46036fb9b
Sohail Charef <charefsohayl@gmail.com>
2023-06-22 13:18:18 -0700

    finally

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle of '#' characters
+ * print_triangle - Prints a triangle
+ *
  * @size: The size of the triangle
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
 
 void print_triangle(int size)
