2c41ba5fba49f803d9535f27d8fa415ab994153a
Sohail Charef <charefsohayl@gmail.com>
2023-06-22 13:14:50 -0700

    finally

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,15 +12,19 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= i; j++)
-			_putchar('#');
-		_putchar('\n');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				if ((i + j) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
