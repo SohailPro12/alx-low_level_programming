35fc7cd48d4e581112070ff6dfccc73ba23fd1be
Sohail Charef <charefsohayl@gmail.com>
2023-06-24 04:14:30 -0700

    Surviving

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned int num1 = 0, num2 = 1, next;
+
+	for (i = 0; i < 50; i++)
+	{
+		next = num1 + num2;
+		printf(", %lu", next);
+
+		num1 = num2;
+		num2 = next;
+
+		if (i == 49)
+			printf('\n');
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
+
