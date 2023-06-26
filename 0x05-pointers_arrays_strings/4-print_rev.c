3d8388eb3ebac853c19a9544a4ec2afcdc2e9a7a
Sohail Charef <charefsohayl@gmail.com>
2023-06-26 04:11:44 -0700

    Another one for poinnters

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,15 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 
-	while (s[length] != '\0')
+	while (*s != '\0')
+	{
 		length++;
+		s++;
+	}
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
