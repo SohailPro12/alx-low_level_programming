68be61d035ddd607195c1344f57560ef50332e1d
Sohail Charef <charefsohayl@gmail.com>
2023-06-26 04:41:09 -0700

    Another one for poinnters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int length_of_the_string = 0;
+	int i;
+
+	while (s[length_of_the_string])
+		length_of_the_string++
+
+	for (i = (length_of_the_string - 1) / 2; i != '0\'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
