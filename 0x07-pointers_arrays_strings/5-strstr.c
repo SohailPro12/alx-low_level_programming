5d973e673e9c35d038b6c1d385506e357b87fcec
Sohail Charef <charefsohayl@gmail.com>
2023-07-03 12:45:28 -0700

    Targuist ya dawala

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,23 +10,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	if (*needle == '\0')
-		return (haystack);
-
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (haystack[i] != '\0')
 	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (&haystack[i]);
-		}
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+			j++;
+
+		if (needle[j] == '\0')
+			return (haystack + i);
+
+		i++;
+		j = 0;
 	}
 
 	return (NULL);
