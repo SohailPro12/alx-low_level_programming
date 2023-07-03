03a97080eb60c8dece36fc88c94457cb5a3a8487
Sohail Charef <charefsohayl@gmail.com>
2023-07-03 11:50:27 -0700

    Targuist point

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,25 +11,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
-	int match;
+	int i, j;
 
-	while (*s != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		match = 0;
-		while (*accept != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == *accept)
+			if (s[i] == accept[j])
 			{
 				count++;
-				match = 1;
 				break;
 			}
-			accept++;
 		}
-		if (match == 0)
-			break;
-		s++;
-		accept = accept - count;
+		if (accept[j] == '\0')
+			return (count);
 	}
 
 	return (count);
