15191b5d42289e116de6e329208d691cc0a79170
Sohail Charef <charefsohayl@gmail.com>
2023-07-04 02:42:45 -0700

    Point Targuisst

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
 /**
- * _strstr - Locates a substring.
- * @haystack: Pointer to the string to be searched.
- * @needle: Pointer to the substring to search for.
- *
- * Return: Pointer to the beginning of the located substring in @haystack, or
- *         NULL if the substring is not found.
- */
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0;
+	char *bhaystack;
+	char *pneedle;
 
-	while (haystack[i] != '\0')
+	while (*haystack != '\0')
 	{
-		while (needle[j] != '\0' && haystack[i + j] == needle[j])
-			j++;
+		bhaystack = haystack;
+		pneedle = needle;
 
-		if (needle[j] == '\0')
-			return (haystack + i);
-
-		i++;
-		j = 0;
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
-
-	return (NULL);
+	return (0);
 }
