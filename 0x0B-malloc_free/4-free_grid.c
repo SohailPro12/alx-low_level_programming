67396269b509f46f7a32278ba2e23ac3c03044e0
Sohail Charef <charefsohayl@gmail.com>
2023-07-10 05:50:29 -0700

    Allocation Dynamque

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid
- * @grid: the 2 dimensional grid to free
+ * free_grid - frees the memory allocated for a 2 dimensional grid
+ * @grid: the grid to free
  * @height: the height of the grid
  *
  * Return: void
  */
 void free_grid(int **grid, int height)
 {
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
 	free(grid);
 }
