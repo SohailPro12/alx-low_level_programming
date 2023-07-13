c0b77a7a941499c75aa4bf505d3782a7e57b7fdc
Sohail Charef <charefsohayl@gmail.com>
2023-07-13 04:23:25 -0700

    Mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+void print_int(int n);
+int _atoi(const char *s);
+int _puts(const char *str);
 
 /**
  * main - Multiplies two positive numbers
@@ -15,15 +17,79 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Error\n");
-		return (1);
+		_puts("Error");
+		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
-	printf("%lu\n", result);
+	print_int(result);
+	_puts("");
 
 	return (0);
+}
+
+/**
+ * print_int - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_int(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted
+ *
+ * Return: The converted integer
+ */
+int _atoi(const char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign = -sign;
+		else if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
+		else if (result != 0)
+			break;
+		s++;
+	}
+
+	return (result * sign);
+}
+
+/**
+ * _puts - Prints a string
+ * @str: The string to be printed
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _puts(const char *str)
+{
+	int count = 0;
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		count++;
+	}
+
+	return (count);
 }
