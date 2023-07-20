774ea47eb2e054687410085e9de81db4ec05010c
Sohail Charef <charefsohayl@gmail.com>
2023-07-20 08:36:59 -0700

    Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,8 +11,8 @@
  */
 typedef struct token
 {
-    char *token;
-    void (*f)(char *, va_list);
+	char *token;
+	void (*f)(char *, va_list);
 } token_t;
 
 int sum_them_all(const unsigned int n, ...);
