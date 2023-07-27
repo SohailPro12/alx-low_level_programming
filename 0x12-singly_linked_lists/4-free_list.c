b56aa2429d3bdcc7884d1a61bf0dd914da22d5ed
Sohail Charef <charefsohayl@gmail.com>
2023-07-27 04:34:53 -0700

    Print list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *nnode;
+
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		nnode = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
