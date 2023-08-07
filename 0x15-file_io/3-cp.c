da7f7935df7f469069ea7cdd58dd79870ce9e995
Sohail Charef <charefsohayl@gmail.com>
2023-08-07 11:40:17 -0700

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * main - program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 1 on success 0 on failure
+ */
+int main(int ac, char **av)
+{
+	int F = 0, T = 0;
+	ssize_t b;
+	char buf[READ_BUF_SIZE];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	F = open(av[1], O_RDONLY);
+	if (F == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	T = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (T == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	while ((b = read(F, buf, READ_BUF_SIZE)) > 0)
+		if (write(T, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
+	F = close(F);
+	T = close(T);
+	if (F)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, F), exit(100);
+	if (T)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, F), exit(100);
+
+	return (EXIT_SUCCESS);
+}
