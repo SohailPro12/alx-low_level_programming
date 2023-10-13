7ace453a87c1014fc645be3f48dd035cae04129b
Sohail Charef <charefsohayl@gmail.com>
2023-10-13 03:50:06 -0700

    print doubly list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints doubly-linked list and number
+ * of elements
+ * @h: address of head node
+ *
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	struct dlistint_s *current = h;
+	int i = 0;
+
+	while (current != NULL)
+	{
+		printf("%d", current->n);
+		current = current->next;
+		i++;
+		printf("\n");
+	}
+	return (i);
+}
