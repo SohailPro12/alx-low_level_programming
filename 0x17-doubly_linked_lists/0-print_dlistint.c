32cf6188ecad4753fc54271a45fb6b68aa08b6e9
Sohail Charef <charefsohayl@gmail.com>
2023-10-13 03:52:59 -0700

    print doubly list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,10 +14,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (current != NULL)
 	{
-		printf("%d", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 		i++;
-		printf("\n");
 	}
 	return (i);
 }
