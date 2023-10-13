9c265748fb446b7e6e2df91f4638f16c37b29d9e
Sohail Charef <charefsohayl@gmail.com>
2023-10-13 09:33:53 -0700

    delete  idx

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes a node at a given index.
+ *
+ * @head: A pointer to the head.
+ * @index: The index of the node to delete.
+ *
+ * Return:  success - 1.
+ *         Otherwise - -1.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
+
+	if (current == *head)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
+
+	free(current);
+	return (1);
+}
