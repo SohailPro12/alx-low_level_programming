cdbda923831bb5dc02151135c31e1a840528f436
Sohail Charef <charefsohayl@gmail.com>
2023-10-13 04:30:29 -0700

    add at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,12 +7,19 @@
  *
  * Return: address of new node or NULL
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 
 	if (!head || !newnode)
-		return (newnode ? free(newnode), NULL : NULL);
+	{
+		if (newnode)
+		{
+			free(newnode);
+		}
+		return (NULL);
+	}
 	newnode->n = n;
 	newnode->prev = NULL;
 
@@ -27,5 +34,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = newnode;
 		*head = newnode;
 	}
-	return (NULL);
+	return (newnode);
 }
