836230c9460146b8292eeae554d577ae1e5db2fe
Sohail Charef <charefsohayl@gmail.com>
2023-11-02 07:32:20 -0700

    get the index using the djb2 algorithm

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,11 +4,13 @@
  * key_index - return the index of a key
  * @key: pointer to a char
  * @size: the size of the array
+ * Return: the index
  */
 
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = hash_djb2((unsigned char *)key) % size;
+
 	return (index);
 }
