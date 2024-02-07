7ce80183f48cb3d9a38bd9c1fc9367d7a31484c9
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-02-07 21:46:47 +0100

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+    for (i = 0; i < size; i++){
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (value == array[i]){
+            return (i);
+            printf("Found %d at index: %lu", array[i], i);
+        }
+    }
+    return (-1);
+}
